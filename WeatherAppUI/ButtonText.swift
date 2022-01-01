e58b1a25cc9da37e16a4d6c773d3dfbe1fe06f05
hossain.iqminds <Dev2021#!>
2022-01-01 13:02:45 +0600

    ios native basic screen design

--- WeatherAppUI/ButtonText.swift
+++ WeatherAppUI/ButtonText.swift
@@ -1,8 +1,11 @@
-//
-//  ButtonText.swift
-//  WeatherAppUI
-//
-//  Created by Imran Sk on 1/1/22.
-//
+import SwiftUI
 
-import Foundation
+struct ButtonText: View {
+    var body: some View {
+        Text("Change Day time")
+            .frame(width: 250, height: 50)
+            .font(.system(size: 22,weight: .medium))
+            .background(.white)
+            .cornerRadius(10.0)
+    }
+}
--- WeatherAppUI/ContentView.swift
+++ WeatherAppUI/ContentView.swift
@@ -1,21 +1,114 @@
-//
-//  ContentView.swift
-//  WeatherAppUI
-//
-//  Created by Imran Sk on 28/12/21.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+     
+        ZStack{
+            BackgroundColor();
+                
+                VStack {
+                    
+                    CityNameview()
+                    
+                    TodayTemperature()
+                    Spacer()
+                    
+                    HStack(spacing: 15){
+                        
+                        ForcastWeather(daysString: "SUN", imageString: "cloud.sun.fill", temString: 35)
+                        ForcastWeather(daysString: "MON", imageString: "sun.max.fill", temString: 45)
+                        ForcastWeather(daysString: "TUE", imageString: "cloud.fill", temString: 25)
+                        ForcastWeather(daysString: "WED", imageString: "cloud.drizzle.fill", temString: 85)
+                        ForcastWeather(daysString: "THU", imageString: "cloud.sun.fill", temString: 95)
+                        ForcastWeather(daysString: "FRI", imageString: "cloud.sun.fill", temString: 25)
+                            
+                    }
+                    
+                    Spacer()
+                    Button {
+                        print("pressed")
+                        
+                    }label:{
+                        
+                        ButtonText()
+                    }
+                    
+                    Spacer()
+                        
+                }
+            
+            
+        }
+    
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+.previewInterfaceOrientation(.portraitUpsideDown)
+    }
+}
+
+struct ForcastWeather: View {
+    
+    var daysString:String
+    var imageString:String
+    var temString:Int
+    
+    var body: some View {
+        
+            VStack {
+                Text("\(daysString)").foregroundColor(.white).font(.system(size: 20,weight: .medium))
+                Image(systemName: imageString).resizable().renderingMode(.original)
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 40
+                           , height: 40 )
+                Text("\(temString)°").foregroundColor(.white).font(.system(size: 20,weight: .medium))
+            }
+        
+    }
+}
+
+struct BackgroundColor: View {
+    
+    
+    var body: some View {
+        LinearGradient.init(
+            gradient: Gradient(colors: [.blue,Color("ColorLightBlue")]),
+            startPoint: UnitPoint.topLeading,
+            endPoint: UnitPoint.bottomTrailing)
+            .ignoresSafeArea()
+    }
+}
+
+
+struct CityNameview: View {
+    var body: some View {
+        Text("Capertino,CA")
+            .font(.system(size: 36 ,weight: .medium  ,design: .default))
+            .foregroundColor(.white)
+            .padding()
+    }
+}
+
+struct TodayTemperature: View {
+    var body: some View {
+        VStack(spacing: 8){
+            
+            Image(systemName: "cloud.sun.fill")
+                .resizable()
+                .renderingMode(.original)
+                .aspectRatio(contentMode: ContentMode.fit)
+                .frame(width: 180, height: 180)
+            
+            Text("76°")
+                .font(.system(size: 70,weight: .medium))
+                .foregroundColor(.white)
+            
+        }
     }
 }
